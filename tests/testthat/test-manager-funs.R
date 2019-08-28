df50961e779905d0af2edb63363db4a1a4b1366c
Are Edvardsen <biorakel@gmail.com>
2019-08-28 12:17:00 +0200

    portable, hopefully

--- tests/testthat/test-manager-funs.R
+++ tests/testthat/test-manager-funs.R
@@ -5,23 +5,20 @@ currentConfig <- Sys.getenv("R_RAP_CONFIG_PATH")
 Sys.setenv(R_RAP_CONFIG_PATH="")
 tempdir <- tempdir()
 
-conf <- rapbase::getConfig(fileName = "rapbaseConfig.yml")
-archiveDir <- conf$r$raplog$archiveDir
-
 test_that("error is provided when config path env var is empty", {
   expect_error(raplogManager())
 })
 
 Sys.setenv(R_RAP_CONFIG_PATH=tempdir)
+file.copy(system.file("rapbaseConfig.yml", package = "rapbase"), tempdir)
+conf <- rapbase::getConfig(fileName = "rapbaseConfig.yml")
+archiveDir <- conf$r$raplog$archiveDir
 
-logs <- c("mtcars.csv")
-rio::export(mtcars, file.path(tempdir, logs))
 raplogManager()
 
 test_that("for now, just that archiveDir is created", {
   expect_true(dir.exists(file.path(tempdir, archiveDir)))
 })
-
 
 # Restore instance
 Sys.setenv(R_RAP_CONFIG_PATH=currentConfig)
